9fd092c1dd9e73b67ccb2141475c7bca72f0f637
shuzoyrk-bit <shuzo.yrk@gmail.com>
2026-06-19 12:04:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,15 +13,28 @@ app.ws('/ws', (ws, req) => {
   connects.push(ws)
 
   ws.on('message', (message) => {
-    console.log('Received:', message)
-
-    connects.forEach((socket) => {
+      connects.forEach((socket) => {
       if (socket.readyState === 1) {
-        // Check if the connection is open
         socket.send(message)
       }
     })
-  })
+  const { text } = JSON.parse(message)
+  let reply = "メッセージありがとうございます！キーワードを入れると返信が返ってくるかも…？"
+  if(text === "犬"){
+    reply = "ワン、ワワン。 ワン！ワンワンワンワン！！！キャオンクォン！クゥーンクンクンクーンキャイーンワオワオワオ！"
+  }
+  if(text === "電大"){
+    reply = "東京電機大学（とうきょう でんきだいがく、英語: Tokyo Denki University）は、日本の私立大学。東京都足立区千住旭町に本部を置く。1907年創立、1949年大学設置。略称は電大、電機大、TDU。"
+  }
+  const botMessage = {
+    id: "BOT",
+    text: reply
+  }
+    connects.forEach((socket) => {
+      if(socket.readyState === 1){
+        socket.send(JSON.stringify(botMessage))
+      }
+    }) 
 
   ws.on('close', () => {
     connects = connects.filter((conn) => conn !== ws)
